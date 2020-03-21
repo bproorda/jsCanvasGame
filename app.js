f74f6b1cafd4cead03a9ec4af4aa939e35565910
Brennan <bproorda@gmail.com>
2020-03-21 13:05:53 -0500

    paddle works!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 console.log('app.js is running');
 
+//global variables
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var x = canvas.width/2;
@@ -9,6 +10,35 @@ var ballRadius = 10;
 var dx = 2;
 var dy = -2;
 var ballColor = 'gray';
+var paddleColor = 'blue';
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+//event listeners
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+//player controls
+function keyDownHandler(e) {
+  if(e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  }
+  else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  }
+  else if(e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -18,18 +48,47 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
     ballColor = 'red';
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  if(y + dy < ballRadius) {
+    ballColor = 'green';
     dy = -dy;
-    ballColor = 'blue';
+  } else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert('GAME OVER');
+      document.location.reload();
+      clearInterval(interval); // Needed for Chrome to end game
+    }
+  }
+  if(rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width){
+      paddleX = canvas.width - paddleWidth;
+    }
+  }
+  else if(leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0){
+      paddleX = 0;
+    }
   }
   x += dx;
   y += dy;
 }
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
